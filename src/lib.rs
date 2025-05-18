6e215626fb5f921209520d0fc828963f513a8295
Parker Timmerman <parker@parkertimmerman.com>
2025-05-18 14:49:31 -0400

    re-implement std-rules with the rules-sdk

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,61 @@
-pub mod rules;
-
-wit_bindgen::generate!({
-    world: "rule-set",
-    path: "pb-wit/wit"
-});
-
-pub struct MyWaker;
+use futures::{StreamExt, future::BoxFuture};
+use pb_rules_sdk::{futures::ByteStreamWrapper, rules::Rule};
 
 struct StdRules;
 
-impl exports::pb::rules::resolver::Guest for StdRules {
-    fn additional_interest_glob() -> Option<_rt::String> {
-        let location = pb::rules::logging::Location {
-            file_path: None,
-            line: None,
-        };
-        pb::rules::logging::event(
-            pb::rules::logging::Level::Info,
-            "additional interest",
-            &location,
-            &[],
-        );
+impl pb_rules_sdk::resolver::Resolver for StdRules {
+    fn additional_interest_glob() -> Option<String> {
+        tracing::info!("logging from additional interests glob");
         Some("pb.toml".into())
     }
 
     fn resolve_target(
-        _file: exports::pb::rules::resolver::File,
-    ) -> Result<_rt::Vec<exports::pb::rules::resolver::Target>, _rt::String> {
-        let location = pb::rules::logging::Location {
-            file_path: None,
-            line: None,
-        };
-        pb::rules::logging::event(
-            pb::rules::logging::Level::Info,
-            "resolving target",
-            &location,
-            &[],
-        );
-
+        file: Vec<u8>,
+    ) -> Result<Vec<pb_rules_sdk::exports::pb::rules::resolver::Target>, String> {
+        tracing::debug!("hmmm, we're resolving a target");
         Ok(vec![])
     }
 }
 
-export!(StdRules);
+impl pb_rules_sdk::rules::RuleSet for StdRules {
+    fn rule_set() -> Vec<(String, Box<dyn Rule>)> {
+        vec![("http".to_string(), Box::new(HttpRule))]
+    }
+}
+
+struct HttpRule;
+
+impl pb_rules_sdk::rules::Rule for HttpRule {
+    fn name(&self) -> std::borrow::Cow<'static, str> {
+        "http".into()
+    }
+
+    fn execute(
+        &self,
+        _attrs: pb_rules_sdk::rules::Attributes,
+        context: pb_rules_sdk::pb::rules::context::Ctx,
+    ) -> BoxFuture<'static, Vec<pb_rules_sdk::pb::rules::types::Provider>> {
+        Box::pin(async move {
+            let request = pb_rules_sdk::pb::rules::http::Request {
+                url: "https://jsonplaceholder.typicode.com/comments".into(),
+                headers: vec![],
+            };
+
+            let mut body = Vec::new();
+            let response = context.actions().http().get(&request);
+            let mut body_stream = ByteStreamWrapper::new(response.body());
+
+            while let Some(val) = body_stream.next().await {
+                tracing::info!("new chunk {}, total {}", val.len(), body.len());
+                body.extend_from_slice(&val[..]);
+            }
+
+            let msg = String::from_utf8_lossy(&body[..]);
+            tracing::info!(%msg, "got entire body");
+
+            vec![]
+        })
+    }
+}
+
+pb_rules_sdk::export!(StdRules with_types_in pb_rules_sdk);
